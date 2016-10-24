041dbabb61081b569fcd8ad59717c6b0021f4856
igorwanbarros <igorwanbarros@gmail.com>
2016-10-24 13:03:43 -0300

    implementando componente Breadcrumbs

--- src/Breadcrumbs/BreadcrumbsView.php
+++ src/Breadcrumbs/BreadcrumbsView.php
@@ -15,4 +15,88 @@ class BreadcrumbsView extends ViewAbstract
      * @var string
      */
     protected $template = 'templates/%s/breadcrumbs.php';
+
+    /**
+     * @type array
+     */
+    protected $itens = [];
+
+    /**
+     * @type bool
+     */
+    protected $active = false;
+
+
+    /**
+     * @param array $itens
+     */
+    public function __construct(array $itens = [])
+    {
+        $this->setItens($itens);
+        $this->addAttribute('class', 'breadcrumb');
+    }
+
+
+    public function getVars()
+    {
+        return ['breadcrumbs' => $this];
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getItens()
+    {
+        return $this->itens;
+    }
+
+
+    /**
+     * @param $itens
+     *
+     * @return $this
+     */
+    public function setItens(array $itens)
+    {
+        $this->itens = [];
+
+        foreach ($itens as $url => $linkName) {
+            $this->addItem($url, $linkName);
+        }
+
+        return $this;
+    }
+
+
+    public function addItem($url, $linkName)
+    {
+        $this->itens[$url] = $linkName;
+
+        $this->active = $url;
+
+        return $this;
+    }
+
+
+    /**
+     * @return bool|string
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+
+    /**
+     * @param string $active
+     *
+     * @return $this
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
 }
--- src/templates/bootstrap/breadcrumbs.php
+++ src/templates/bootstrap/breadcrumbs.php
@@ -1,1 +1,16 @@
+<ol <?php echo $breadcrumbs->renderAttributes();?>>
+    <?php foreach ($breadcrumbs->getItens() as $url => $linkName):?>
 
+    <?php if ($breadcrumbs->getActive() == $url):?>
+    <li class="active">
+        <?php echo $linkName;?>
+    </li>
+    <?php else:?>
+    <li>
+        <a href="<?php echo $url;?>">
+            <?php echo $linkName;?>
+        </a>
+    </li>
+    <?php endif;?>
+    <?php endforeach;?>
+</ol>
