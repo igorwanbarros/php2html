14a17e020d37190b2b7f99f879bbc4a4b4033d64
igorwanbarros <igorwanbarros@gmail.com>
2016-10-24 12:25:49 -0300

    implementando componente Badges

--- src/Tags/BadgesView.php
+++ src/Tags/BadgesView.php
@@ -2,17 +2,21 @@
 
 namespace Igorwanbarros\Php2Html\Tags;
 
-use Igorwanbarros\Php2Html\ViewAbstract;
-
 /**
  * Class Badges
  * @package Igorwanbarros\Php2Html\Tags
  */
-class BadgesView extends ViewAbstract
+class BadgesView extends LabelView
 {
+    const BOOTSTRAP_DEFAULT = 'badge badge-default';
+    const BOOTSTRAP_PRIMARY = 'badge badge-primary';
+    const BOOTSTRAP_INFO = 'badge badge-info';
+    const BOOTSTRAP_SUCCESS = 'badge badge-success';
+    const BOOTSTRAP_WARNING = 'badge badge-warning';
+    const BOOTSTRAP_DANGER = 'badge badge-danger';
 
-    /**
-     * @var string
-     */
-    protected $template = 'templates/%s/badges.php';
+    public function __construct($label, $class = self::BOOTSTRAP_DEFAULT)
+    {
+        parent::__construct($label, $class);
+    }
 }
