80ebb0de86434591daed2d743e7fbcf0b0efa226
Igor Wanderley Barros <igor.wanderley@hotmail.com>
2019-03-24 17:13:49 -0300

    Update table.php

--- src/templates/bootstrap/table.php
+++ src/templates/bootstrap/table.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-12 col-sm-12">
         <div class="table-responsive">
             <table class="<?php echo $table->getClassTable()?>">
                 <thead>
